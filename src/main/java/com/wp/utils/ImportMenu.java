c1c46f6f828c6bc7c848ad13ece9e08853372a58
zhang <884831651@qq.com>
2017-02-08 13:54:46 +0800

    commit 2/8

--- src/main/java/com/wp/utils/ImportMenu.java
+++ src/main/java/com/wp/utils/ImportMenu.java
@@ -1,28 +1,84 @@
 package com.wp.utils;
 
-import org.springframework.stereotype.Repository;
+import com.wp.food.entity.foods;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.*;
-import java.util.logging.Logger;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zhang on 2017/2/4.
  */
-@Repository
+@Configuration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ImportMenu.class)
+@PropertySource("classpath:app.properties")
 public class ImportMenu {
+    @Autowired
+    Environment env;
+    @Test
     public void importM(){
+      String b=  env.getProperty("a");
+        System.out.println(b+"sssssssssssssssssssssssssss");
         File file=new File("d://menu.doc");
         try {
-            InputStreamReader in= new InputStreamReader(new FileInputStream(file));
-            BufferedReader reader=new BufferedReader(in);
-            String str=null;
-            while ((str=reader.readLine())!=null){
-
-            }
+          //  InputStreamReader in= new InputStreamReader(new FileInputStream(file));
+            FileInputStream in=new FileInputStream(file);
+            WordExtractor wd=new WordExtractor(in);
+            String a=wd.getText();
+            System.out.println(a);
+//            BufferedReader reader=new BufferedReader(in);
+//            String str=null;
+//            while ((str=reader.readLine())!=null){
+//
+//            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void readExcel(File file){
+        try {
+            InputStream in=new FileInputStream(file);
+            try {
+                XSSFWorkbook xssfWorkbook=new XSSFWorkbook(in);
+                foods foods=null;
+                List<foods> list=new ArrayList<foods>();
+                for(int numSheet=0;numSheet<xssfWorkbook.getNumberOfSheets();
+                        numSheet++){
+                    XSSFSheet xssfSheet=xssfWorkbook.getSheetAt(numSheet);
+                    if(xssfSheet==null){
+                        continue;
+                    }
+                    for(int rowNum=1;rowNum<=xssfSheet.getLastRowNum();rowNum++)
+                    {
+                        XSSFRow xssfRow=xssfSheet.getRow(rowNum);
+                        if(xssfRow!=null){
+                            foods=new foods();
+                            foods.setName(String.valueOf(xssfRow.getCell(0)));
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/main/webapp/WEB-INF/views/service/addMany.jsp
+++ src/main/webapp/WEB-INF/views/service/addMany.jsp
@@ -1,1 +1,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<h1>批量导入菜单</h1>
+<form>
+    <input name="file" type="file">
+    <input type="submit">提交
+</form>
+<a href="#">模板下载</a>
