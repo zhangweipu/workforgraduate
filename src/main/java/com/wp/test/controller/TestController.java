6e2acacd4228da5469cf0eb2664393e9df995289
zhangweipu <884831651@qq.com>
2017-04-20 13:49:44 +0800

    food

--- src/main/java/com/wp/test/controller/TestController.java
+++ src/main/java/com/wp/test/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.wp.test.controller;
 
+import com.wp.restuarant.data.food.entity.Foods;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,8 @@ public class TestController {
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index(){
+        Foods foods=new Foods();
+        foods.setDetail("ssss");
         return "test/index";
     }
 }
