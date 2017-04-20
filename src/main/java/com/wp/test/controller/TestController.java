6203c9629dfd05021da8b0ab7f90773ba846cca9
zhangweipu <884831651@qq.com>
2017-04-20 13:47:57 +0800

    添加

--- src/main/java/com/wp/test/controller/TestController.java
+++ src/main/java/com/wp/test/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.wp.test.controller;
 
+import com.wp.restuarant.data.emp.entity.Emp;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,7 +13,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class TestController {
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
+    
     public String index(){
+        Emp emp=new Emp();
+        emp.setId(1);
+        emp.setAge(2);
+        emp.setPower(1);
         return "test/index";
     }
 }
