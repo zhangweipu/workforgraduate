1d9495b23e2bf6807244f090e0760301180b0b13
zhangweipu <884831651@qq.com>
2017-05-11 15:29:52 +0800

    5.11

--- src/main/java/com/wp/restuarant/adminer/controller/FinanceController.java
+++ src/main/java/com/wp/restuarant/adminer/controller/FinanceController.java
@@ -1,5 +1,6 @@
 package com.wp.restuarant.adminer.controller;
 
+import com.wp.restuarant.data.finance.entity.Account;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +11,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/admin/finance")
 public class FinanceController {
+
+    @RequestMapping(value = "initAccount",method = RequestMethod.GET)
+    public String initAccount(){
+        return "service/finance/init";
+    }
+
+    @RequestMapping(value = "/saveAccount",method = RequestMethod.POST)
+    public String initAccount(Account account){
+        return "success";
+    }
+    @RequestMapping(value = "total",method = RequestMethod.GET)
+    public String accountTotal(){
+
+        return "service/finance/total";
+    }
 
     @RequestMapping(value = "/outlist",method = RequestMethod.GET)
     public String outList(){
